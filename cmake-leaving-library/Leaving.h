6d89b0f27d59488d1a74f6db1a98efee7880e628
Raychmony00X <shkunov.artyom@mail.ru>
2026-05-09 22:31:01 +0300

    Add files via upload

---
+++ cmake-leaving-library/Leaving.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#include <string>
+
+#ifdef LEAVINGLIBRARYDYNAMIC_EXPORTS
+#define LEAVINGLIBRARY_API __declspec(dllexport)
+#else
+#define LEAVINGLIBRARY_API __declspec(dllimport)
+#endif
+
+class LEAVINGLIBRARY_API Leaver
+{
+public:
+	std::string leave(std::string name);
+};
